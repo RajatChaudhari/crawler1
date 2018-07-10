2f07764fc20bc69b1b1405ccabd60bea23bb0d50
Rajat Chaudhari <silverprince.v1@gmail.com>
2018-07-10 14:59:18 +0530

    10july

--- tutorial/tutorial/spiders/deepak_spider.py
+++ tutorial/tutorial/spiders/deepak_spider.py
@@ -1,5 +1,10 @@
 import scrapy
-
+from selenium import webdriver
+import pandas as pd
+import numpy as np
+import bs4
+import scrapy as sc
+from scrapy.crawler import CrawlerProcess
 
 class DeepakSpider(scrapy.Spider):
     name = "Deepak"
@@ -8,12 +13,54 @@ class DeepakSpider(scrapy.Spider):
         ]
         
     def parse(self, response):
-        for li in response.css('ul.menu1'):
-            yield {
-                'text': li.css('a.text::text').extract_first()
-            }
-        print(response.text)
-        #next_page = response.css('li.next a::attr(href)').extract_first()
-        '''if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)'''
+        for q in response.css('li a::attr(href)').extract():
+            if q.split(';')[0]=="getSaleAvailRept":
+                
+            if q.split(';')[0]=="getPOSReportForm":
+            if q.split(';')[0]=="monthlyFertilizerSalesAmount":
+            if q.split(';')[0]=="farmerBuying":
+            if q.split(';')[0]=="getsalereportofperiod":
+            if q.split(';')[0]=="getRetailerNotEnteredOs":
+            if q.split(';')[0]=="getRetailerAfterEnteredOsByDate":
+            
+                
+                
+    def SaleReport(url):
+        driver=webdriver.Chrome()
+        driver.get(url)
+    def POSReport(url):
+        driver=webdriver.Chrome()
+        driver.get(url)
+    def FertilizerSaleAmt(url):
+        driver=webdriver.Chrome()
+        driver.get(url)
+    def FarmerBuying(url):
+        driver=webdriver.Chrome()
+        driver.get(url)
+    def SaleReportOfPeriod(url):
+        driver=webdriver.Chrome()
+        driver.get(url)
+    def RetailerNotEnteredOs(url):
+        driver=webdriver.Chrome()
+        driver.get(url)
+    def RetailerAfterEnteredOs(url):
+        driver=webdriver.Chrome()
+        driver.get(url)
+        
+        
+            
+# import scrapy
+
+
+# class DeepakSpider(scrapy.Spider):
+    # name = "Deepak"
+    # start_urls = [
+        # "http://164.100.128.10/mfmsReports/reports",
+        # ]
+        
+    # def parse(self, response):
+       # page = response.url.split("/")[-2]
+       # filename = 'deepak-%s.html' % page
+       # with open(filename, 'wb') as f:
+           # f.write(response.body)
+           # self.log('Saved file %s' % filename)
--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -5,7 +5,7 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
-		]
+        ]
         
     def parse(self, response):
         for quote in response.css('div.quote'):
@@ -14,7 +14,7 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').extract_first(),
                 'tags': quote.css('div.tags a.tag::text').extract(),
             }
-		next_page = response.css('li.next a::attr(href)').extract_first()
+        next_page = response.css('li.next a::attr(href)').extract_first()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
